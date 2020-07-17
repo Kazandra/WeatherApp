9928abf0cd8fbb37b80c61f8db48d91bbaf97ff0
Kazandra <kazandra.zelaya@gmail.com>
2020-07-16 23:49:55 -0700

    Updates

---
+++ precache-manifest.24547842d742f17afcc90b5a60c284e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3cc3e520ea2e52127f8f3a547e9aa29",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "e2a1d555f0c53b999059",
+    "url": "/WeatherApp/static/css/main.ac8835b9.chunk.css"
+  },
+  {
+    "revision": "5fae6b1de25c9e99106f",
+    "url": "/WeatherApp/static/js/2.0258b798.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.0258b798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a1d555f0c53b999059",
+    "url": "/WeatherApp/static/js/main.e6a765ff.chunk.js"
+  },
+  {
+    "revision": "5fdeb778b69e8ff58bbb",
+    "url": "/WeatherApp/static/js/runtime-main.8c7ddbed.js"
+  },
+  {
+    "revision": "79af64116054310634e920c815e1ac0e",
+    "url": "/WeatherApp/static/media/cold.79af6411.png"
+  },
+  {
+    "revision": "2e30bf66defcf973d71baf2d5e3f89dd",
+    "url": "/WeatherApp/static/media/hot.2e30bf66.png"
+  },
+  {
+    "revision": "edddbbdd1caa1e327ee542f5f5732e19",
+    "url": "/WeatherApp/static/media/mid.edddbbdd.png"
+  }
+]);
